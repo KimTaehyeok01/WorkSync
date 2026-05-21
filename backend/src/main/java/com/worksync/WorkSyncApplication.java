2164d03fcbb028a702d6de4867894bf449329f34
KimTaehyeok01 <taehyeok1124@gmail.com>
2026-05-21 10:32:40 +0900

    .

--- backend/src/main/java/com/worksync/WorkSyncApplication.java
+++ backend/src/main/java/com/worksync/WorkSyncApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableJpaAuditing
+@EnableJpaAuditing
 public class WorkSyncApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkSyncApplication.class, args);
