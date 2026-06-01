b52ccbe7de49d895bd8a8aa152cac3cebb77c944
soy0sohee <soy0sohee@gmail.com>
2026-06-01 09:33:05 +0900

    feature/frontend-board 병합

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -8,7 +8,7 @@ import BoardListPage from "../domains/board/pages/BoardListPage";
 import BoardCreatePage from "../domains/board/pages/BoardCreatePage";
 import BoardDetailPage from "../domains/board/pages/BoardDetailPage";
 import BoardUpdatePage from "../domains/board/pages/BoardUpdatePage";
-import OrganizationPage from "../domains/organization/pages/OrganizationPage";
+import OrganizationListPage from "../domains/organization/pages/OrganizationListPage";
 import EmployeeCreatePage from "../domains/organization/pages/EmployeeCreatePage";
 import EmployeeEditPage from "../domains/organization/pages/EmployeeEditPage";
 import TaskListPage from "../domains/task/pages/TaskListPage";
@@ -58,7 +58,7 @@ export const router = createBrowserRouter([
       { path: "board/qna", Component: BoardListPage },
       { path: "board/:boardId/:postId", Component: BoardDetailPage },
       { path: "board/edit/:boardId/:postId", Component: BoardUpdatePage },
-      { path: "organization", Component: OrganizationPage },
+      { path: "organization", Component: OrganizationListPage },
       { path: "organization/employee-add", Component: EmployeeCreatePage },
       { path: "tasks", Component: TaskListPage },
       { path: "tasks/new", Component: TaskCreatePage },
